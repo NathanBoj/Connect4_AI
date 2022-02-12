e6e1bf94c30cb01127c0fb279bd5df0cef351dc9
NathanBoj <nathan.bojczuk@ontariotechu.net>
2022-02-12 17:26:54 -0500

    Junit testing

--- Junit/Tester.java
+++ Junit/Tester.java
@@ -4,14 +4,87 @@ import static org.junit.Assert.*;
 
 public class Tester {
 
-    @Test
-    public void testWin() {
+    @Test //Horizontal Win
+    public void H_testWin() {
         Main tester = new Main();
-        assertEquals(true, tester.connect4(new int[][]{{0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 1, 1, 1, 1},}, 1));
+        assertTrue("Horizontal Win",tester.connect4(
+                new int[][]{{0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 1, 1, 1, 1},}, 1));
+    }
+
+    @Test //Vertical Win, works with >4 tokens
+    public void V_testWin() {
+        Main tester = new Main();
+        assertTrue("Vertical Win", tester.connect4(
+                new int[][]{{0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 2, 0, 0, 0, 0},
+                            {0, 0, 2, 0, 0, 0, 0},
+                            {0, 0, 2, 0, 0, 0, 0},
+                            {0, 0, 2, 0, 0, 0, 0},
+                            {0, 0, 2, 0, 0, 0, 0},}, 2));
+    }
+
+    @Test //Diagonal Win
+    public void D_testWin() {
+        Main tester = new Main();
+        assertTrue("Diagonal Win", tester.connect4(
+                new int[][]{{0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 1, 0},
+                            {0, 0, 0, 0, 1, 0, 0},
+                            {0, 0, 0, 1, 0, 0, 0},
+                            {0, 0, 1, 0, 0, 0, 0},}, 1));
+    }
+
+    @Test //False Win
+    public void test_NoWin() {
+        Main tester = new Main();
+        assertFalse("False Win", tester.connect4(
+                new int[][]{{0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0},
+                            {0, 0, 1, 1, 2, 1, 0},}, 1));
+    }
+
+    @Test //Test to place a token at an already full column
+    public void FullColumn_test() {
+        Main tester = new Main();
+        assertEquals("Column full!", tester.placeToken("B",1,
+                new int[][]{{0, 1, 0, 0, 0, 0, 0},
+                            {0, 2, 0, 0, 0, 0, 0},
+                            {0, 1, 0, 0, 0, 1, 0},
+                            {0, 1, 0, 0, 2, 1, 0},
+                            {0, 1, 0, 1, 1, 2, 0},
+                            {0, 2, 1, 2, 2, 1, 0}}));
+    }
+
+    @Test //Test a regular move
+    public void NormalMove_test() {
+        Main tester = new Main();
+        assertEquals("", tester.placeToken("D",2,
+                new int[][]{{0, 1, 0, 0, 0, 0, 0},
+                            {0, 2, 0, 0, 0, 0, 0},
+                            {0, 1, 0, 0, 0, 1, 0},
+                            {0, 1, 0, 0, 2, 1, 0},
+                            {0, 1, 0, 1, 1, 2, 0},
+                            {0, 2, 1, 2, 2, 1, 0}}));
+    }
+
+    @Test //Test invalid input
+    public void InvalidInput_test() {
+        Main tester = new Main();
+        assertEquals("Invalid Move!", tester.placeToken("H",1,
+                new int[][]{{0, 1, 0, 0, 0, 0, 0},
+                            {0, 2, 0, 0, 0, 0, 0},
+                            {0, 1, 0, 0, 0, 1, 0},
+                            {0, 1, 0, 0, 2, 1, 0},
+                            {0, 1, 0, 1, 1, 2, 0},
+                            {0, 2, 1, 2, 2, 1, 0}}));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -15,48 +15,60 @@ public class Main {
         System.out.println("A   B   C   D   E   F   G");
     }
 
-    public static void placeToken(String move,int player,int[][] board) {
+    public static String placeToken(String move,int player,int[][] board) {
 
         int row = 5;
         int column = 0;
+        String message = "";
 
         switch (move) {
             case "A":
+            case "a":
                 column = 0;
                 break;
             case "B":
+            case "b":
                 column = 1;
                 break;
             case "C":
+            case "c":
                 column = 2;
                 break;
             case "D":
+            case "d":
                 column = 3;
                 break;
             case "E":
+            case "e":
                 column = 4;
                 break;
             case "F":
+            case "f":
                 column = 5;
                 break;
             case "G":
+            case "g":
                 column = 6;
                 break;
+            default:
+                return"Invalid Move!";
         }
 
         if (board[0][column] != 0){
-            System.out.println("Column full");
+            message = "Column full!";
+            //skip players turn
         }else {
             while (board[row][column] != 0) {
                 row--;
             }
-
             board[row][column] = player;
         }
+        return message;
     }
 
     public static boolean connect4(int[][]board, int player){
 
+        //Horizontal Win
         for(int row = 0; row<6; row++){
             for (int col = 0;col < 7 - 3;col++){
                 if (board[row][col] == player   &&
@@ -68,6 +80,7 @@ public class Main {
             }
         }
 
+        //Vertical Win
         for(int row = 0; row < 6 - 3; row++){
             for(int col = 0; col < 7; col++){
                 if (board[row][col] == player   &&
@@ -79,6 +92,7 @@ public class Main {
             }
         }
 
+        //Diagonal Win
         for(int row = 3; row < 6; row++){
             for(int col = 0; col < 7 - 3; col++){
                 if (board[row][col] == player   &&
@@ -89,7 +103,6 @@ public class Main {
                 }
             }
         }
-
         for(int row = 0; row < 6 - 3; row++){
             for(int col = 0; col < 7 - 3; col++){
                 if (board[row][col] == player   &&
@@ -103,15 +116,14 @@ public class Main {
         return false;
     }
 
-
     public static void main(String[] args) {
 
         int[][] board = { {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0},};
+                        {0,0,0,0,0,0,0},
+                        {0,0,0,0,0,0,0},
+                        {0,0,0,0,0,0,0},
+                        {0,0,0,0,0,0,0},
+                        {0,0,0,0,0,0,0},};
 
         showBoard(board);
 
@@ -119,18 +131,21 @@ public class Main {
 
         boolean play1 = false;
         boolean play2 = false;
+        int turn = 0;
 
         while(!(play1 || play2)) {
 
+            System.out.println("Turn #" + ++turn + ":");
+
             System.out.println("P1's turn:");
             String input1 = scan.nextLine();
-            placeToken(input1,1,board);
+            System.out.println(placeToken(input1,1,board));
             showBoard(board);
             play1 = connect4(board,1);
 
             System.out.println("P2's turn:");
             String input2 = scan.nextLine();
-            placeToken(input2,2,board);
+            System.out.println(placeToken(input2,2,board));
             showBoard(board);
             play2 = connect4(board,2);
         }
